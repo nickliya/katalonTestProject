efeaca1173018a061e4707168e32e5cbb4145378
gaomin <g123456m>
2018-07-24 14:07:43 +0800

    1

---
+++ Test Suites/suite1.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>suite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-07-24T13:14:00</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>794a5b0c-e00b-4b7f-b58c-cc2078277670</testSuiteGuid>
+   <testCaseLink>
+      <guid>1077a394-d4b1-48f3-981e-3a4491c4fadd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/gmmtest/case3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
